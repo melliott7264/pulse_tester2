ae59bb4781039bd32fb55fa795ee4dc4fba60cc4
Mark Elliott <markelliottva@gmail.com>
2022-12-29 11:40:01 -0500

    added script changes to style What We Do section

--- cssmod.js
+++ cssmod.js
@@ -2,17 +2,36 @@
 // must also center #do-graphic on #what-we-do-container
 jQuery(document).ready(function ($) {
   console.log("Calculating height and padding-top for #what-we-do-conatiner");
-  $("#what-we-do-container").css({
-    height: $("#bar-behind").outerHeight() * 1.6 + "px", // set height to 160% of #bar-behind
-  });
-  $("#what-we-do-container").css({
-    "padding-top": $("#bar-behind").outerHeight() * 0.30 + "px", // to center #bar-behind in #what-we-do-container
-  });
-  $("#do-graphic").css({
-    top:
-      ($("#what-we-do-container").outerHeight() -
-        $("#do-graphic").outerHeight()) /
-        2 +
-      "px", // to center #do-graphic in #what-we-do-container
-  });
+  if ($("#bar-behind").outerHeight() * 1.6 >= $("#do-graphic").outerHeight()) {
+    $("#what-we-do-container").css({
+      height: $("#bar-behind").outerHeight() * 1.6 + "px", // set height to 160% of #bar-behind
+    });
+    $("#what-we-do-container").css({
+      "padding-top": $("#bar-behind").outerHeight() * 0.3 + "px", // to center #bar-behind in #what-we-do-container
+    });
+    $("#do-graphic").css({
+      top:
+        ($("#what-we-do-container").outerHeight() -
+          $("#do-graphic").outerHeight()) /
+          2 +
+        "px", // to center #do-graphic in #what-we-do-container
+    });
+  } else {
+    $("#what-we-do-container").css({
+      height:
+        $("#do-graphic").outerHeight() +
+        20 -
+        ($("#do-graphic").outerHeight() - $("#bar-behind").outerHeight()) / 2 +
+        "px",
+      "padding-top":
+        ($("#do-graphic").outerHeight() - $("#bar-behind").outerHeight()) / 2 +
+        "px", // set height to #do-graphic, center #bar-behind in #what-we-do-container
+    });
+    $("#do-graphic").css({
+      top: "10px", // to center #do-graphic in #what-we-do-container
+    });
+  }
 });
+$(window).resize(function () {
+    location.reload();
+  });
